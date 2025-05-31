3b79d28ab0105189e3c8db2d9109c57c7ad9be6d
alex-faryna <109907113+alex-faryna@users.noreply.github.com>
2025-05-31 18:00:05 +0200

    added sample router/navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
 import './App.css'
 import {useShopStore} from "./state/state.ts";
 import {useShallow} from "zustand/react/shallow";
+import {Link, Route, Routes, useNavigate, useParams} from "react-router";
+import {useEffect} from "react";
+import {backButton} from "@telegram-apps/sdk-react";
+
+function Profile() {
+
+  return <span>Hello profile</span>
+}
+
+function Home() {
+  return <div>
+    <Link to="/item/34">Item</Link>
+    <span>Heello home</span>
+  </div>
+}
+
+function Item() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (backButton.show.isAvailable()) {
+      backButton.show();
+    }
+
+    const offClick = backButton.onClick(() => navigate('/'));
+
+    return () => {
+      if (backButton.hide.isAvailable()) {
+        offClick();
+        backButton.hide();
+      }
+    }
+  }, []);
+
+  return <span>Heello item { params.itemId }</span>
+}
 
 function App() {
   const { bears, increase } = useShopStore(
     useShallow((state) => ({ bears: state.bears, increase: state.increase })),
   );
 
-  return (
+  return <Routes>
+    <Route index element={<Home />} />
+    <Route path="item/:itemId" element={<Item />} />
+    <Route path="profile" element={<Profile /> as React.ReactNode} />
+  </Routes>;
+
+  /* return (
     <>
       <h1>Bears: { bears }</h1>
       <div className="card">
@@ -16,7 +59,7 @@ function App() {
         </button>
       </div>
     </>
-  )
+  ) */
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {backButton, closingBehavior, init} from "@telegram-apps/sdk-react";
+import {BrowserRouter} from "react-router";
+
+init();
+
+if (closingBehavior.mount.isAvailable()) {
+  console.log('mount');
+  closingBehavior.mount();
+}
+
+if (closingBehavior.enableConfirmation.isAvailable()) {
+  // closingBehavior.enableConfirmation();
+}
+
+if (backButton.mount.isAvailable()) {
+  backButton.mount();
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter> as React.ReactNode
 )
